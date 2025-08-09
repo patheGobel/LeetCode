ef20ec3f0ae3095dd6e632285d395ee658d7d837
patheGobel <pathegba@gmail.com>
2025-08-09 18:30:41 +0000

    Ajout des premiers exos LeetCode

---
+++ rectangleArea.c
@@ -0,0 +1,31 @@
+// Online C compiler to run C program online
+int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2); 
+#include <stdio.h>
+
+int main() {
+    int bx1,  by1,  bx2,  by2;
+    printf("Donner les coordonnées:\n");
+    scanf("%d %d %d %d",&bx1,  &by1,  &bx2,  &by2);
+    printf("Aire Totale = %d", computeArea(1, 1, 5, 3, bx1, by1, bx2, by2) );
+    return 0;
+}
+
+int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+    int La, la, Lb, lb, Lc, lc;
+    La = ax2 - ax1; 
+    la = ay2 - ay1;
+    Lb = bx2 - bx1;
+    lb = by2 - by1;
+    Lc = ax2 - bx1;
+    lc = by2 - ay1;
+
+    if ( La * la == 0 ){ return Lb*lb; }
+
+    else if ( Lb * lb == 0 ){ return La * la; }
+ 
+    else if (La * la == 0 && Lb * lb == 0 ){ return 0; }
+
+    else if ( (bx1 > ax2) || (by1 > ay2) || (ax1 > bx2) || (ay1 > by2) ){ return (La * la) + (Lb * lb); }
+
+    else { return (La * la) + (Lb*lb) - (Lc*lc); }
+}
