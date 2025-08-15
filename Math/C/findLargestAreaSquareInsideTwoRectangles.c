2272f16f906aaa49877cd97f92830fec1ac3964a
patheGobel <pathegba@gmail.com>
2025-08-15 00:36:47 +0000

    Resolution du problème

--- Math/C/findLargestAreaSquareInsideTwoRectangles.c
+++ Math/C/findLargestAreaSquareInsideTwoRectangles.c
@@ -1,51 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+// Prototype de la fonction
 long long largestSquareArea(int **bottomLeft, int bottomLeftSize, int *bottomLeftColSize, int **topRight, int topRightSize, int *topRightColSize);
-long long largestSquareArea(int **bottomLeft, int bottomLeftSize, int *bottomLeftColSize, int **topRight, int topRightSize, int *topRightColSize)
-{
-    int delta_x;
-    for (int i = 0; i < (bottomLeftSize / (*bottomLeftColSize)); i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            printf("%d", (topRight[i][j] < topRight[i + 1][j]) ? topRight[i][j] : topRight[i + 1][j]);
-            printf("%d", (bottomLeft[i][j] > bottomLeft[i + 1][j]) ? bottomLeft[i][j] : bottomLeft[i + 1][j]);
-            // delta_x = (topRight[i][j] > topRight[i+1][j]) ? topRight[i][j] : topRight[i+1][j]-(bottomLeft[i][j] > bottomLeft[i+1][j]) ? bottomLeft[i][j] : bottomLeft[i+1][j];
-            if (delta_x < 0)
-            {
-                return 0;
-                break;
-            }
-            else
-            {
-                0;
+
+long long largestSquareArea(int **bottomLeft, int bottomLeftSize, int *bottomLeftColSize, int **topRight, int topRightSize, int *topRightColSize) {
+    long long max_square_area = 0; // Variable pour stocker la plus grande aire
+
+    // Utilisation de bottomLeftSize pour la boucle, rendant le code générique
+    for (int i = 0; i < bottomLeftSize; i++) {
+        for (int j = i + 1; j < bottomLeftSize; j++) {
+            // Calcul du chevauchement horizontal (dx)
+            int dx = min(topRight[i][0], topRight[j][0]) - max(bottomLeft[i][0], bottomLeft[j][0]);
+
+            // Calcul du chevauchement vertical (dy)
+            int dy = min(topRight[i][1], topRight[j][1]) - max(bottomLeft[i][1], bottomLeft[j][1]);
+            
+            // Vérifier s'il y a un chevauchement
+            if (dx > 0 && dy > 0) {
+                // Trouver le côté du carré de chevauchement (le plus petit côté du rectangle de chevauchement)
+                int overlap_side = min(dx, dy);
+
+                // Calculer l'aire du carré de chevauchement
+                long long current_area = (long long)overlap_side * overlap_side;
+
+                // Mettre à jour la plus grande aire trouvée
+                if (current_area > max_square_area) {
+                    max_square_area = current_area;
+                }
             }
         }
     }
-}
-
-int main()
-{
-    int bottomLeft[3][2] = {{1, 1}, {2, 2}, {3, 1}};
-
-    // Taille totale du tableau (nombre d'éléments)
-    int bottomLeftSize = sizeof(bottomLeft) / sizeof(bottomLeft[0][0]);
-
-    // Nombre de colonnes
-    int bottomLeftColSize = sizeof(bottomLeft[0]) / sizeof(bottomLeft[0][0]);
-
-    int topRight[3][2] = {{3, 3}, {4, 4}, {6, 6}};
-
-    // Taille totale du tableau (nombre d'éléments)
-    int topRightSize = sizeof(topRight) / sizeof(topRight[0][0]);
-
-    // Nombre de colonnes
-    int topRightColSize = sizeof(topRight[0]) / sizeof(topRight[0][0]);
-    /*
-        printf("bottomLeftSize : %d\n", bottomLeftSize);
-        printf("bottomLeftColSize : %d\n", bottomLeftColSize);
-        printf("topRightSize : %d\n", topRightSize);
-        printf("topRightColSize : %d\n", topRightColSize);
-    */
-    largestSquareArea(bottomLeft, bottomLeftSize, &bottomLeftColSize, topRight, topRightSize, &topRightColSize);
+    
+    return max_square_area;
 }
